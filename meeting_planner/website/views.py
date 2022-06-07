bd97c1a5ca51e3aec0ea3ab743c9cf46e876c4fb
PeaceChanpornpakdee <yothinpeace@gmail.com>
2022-06-07 12:54:09 +0700

    Sending data from view to template

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def welcome(request):
-    return render(request, "website/welcome.html")
+    return render(request, "website/welcome.html", {"message": "This data was sent from view to template", 'name':"Peace", 'age':20})
 
 def about(request):
     return HttpResponse("My name is Peace")
