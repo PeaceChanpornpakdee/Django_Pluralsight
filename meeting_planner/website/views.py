a77c58ab72dc3e1185718e9aa4821c924c57d7ec
PeaceChanpornpakdee <yothinpeace@gmail.com>
2022-06-07 14:16:30 +0700

    Link

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -5,8 +5,8 @@ from meetings.models import Meeting
 # Create your views here.
 
 def welcome(request):
-    return render(request, "website/welcome.html", {"message": "This data was sent from view to template", 'name':"Peace", 'age':20
-    , "num_meetings": Meeting.objects.count()})
+    return render(request, "website/welcome.html", {"message": "This data was sent from view to template", 'name':"Peace", 'age':20, 
+    "meetings": Meeting.objects.all()})
 
 def about(request):
     return HttpResponse("My name is Peace")
