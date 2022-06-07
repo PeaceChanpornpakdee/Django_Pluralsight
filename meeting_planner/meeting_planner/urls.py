10227ebf2eb8915386cf50b073e45eb40522b811
PeaceChanpornpakdee <yothinpeace@gmail.com>
2022-06-07 12:29:51 +0700

    Move Welcome and About page to Website App

--- meeting_planner/meeting_planner/urls.py
+++ meeting_planner/meeting_planner/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from meetings.views import welcome, about
+from website.views import welcome, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
