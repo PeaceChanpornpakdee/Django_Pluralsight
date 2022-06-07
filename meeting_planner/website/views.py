61fab0e7d8b9593d3bd0496c183825ee714dc801
PeaceChanpornpakdee <yothinpeace@gmail.com>
2022-06-07 13:26:37 +0700

    Meeting Detail Page

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from meetings.models import Meeting
 
 # Create your views here.
 
 def welcome(request):
-    return render(request, "website/welcome.html", {"message": "This data was sent from view to template", 'name':"Peace", 'age':20})
+    return render(request, "website/welcome.html", {"message": "This data was sent from view to template", 'name':"Peace", 'age':20
+    , "num_meetings": Meeting.objects.count()})
 
 def about(request):
     return HttpResponse("My name is Peace")
